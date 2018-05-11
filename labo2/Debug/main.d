da1b7595f6aa62eaa875d87ae5b119b2374dff5c
juan199 <juancho0_8@hotmail.com>
2018-05-10 18:56:17 -0600

    Bla bla bla

--- labo2/Debug/main.d
+++ labo2/Debug/main.d
@@ -1,61 +1,61 @@
 # FIXED
 
 main.obj: ../main.cpp
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/msp.h
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/msp432p401r.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdint.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config_site
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__libcxx_extra.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h
-main.obj: /home/sama/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/msp.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/msp432p401r.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdint.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config_site
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__libcxx_extra.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+main.obj: /home/juan/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h
 main.obj: ../main.hpp
 main.obj: ../Scheduler.hpp
 main.obj: ../Task.hpp
 main.obj: ../Mailbox.hpp
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdlib.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdlib.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h
-main.obj: /home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdlib.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdlib.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h
+main.obj: /home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h
 main.obj: ../LED.hpp
 
 ../main.cpp: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/msp.h: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/msp432p401r.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdint.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config_site: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__libcxx_extra.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
-/home/sama/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/msp.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/msp432p401r.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdint.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__config_site: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/__libcxx_extra.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+/home/juan/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
 ../main.hpp: 
 ../Scheduler.hpp: 
 ../Task.hpp: 
 ../Mailbox.hpp: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdlib.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdlib.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h: 
-/home/sama/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/libcxx/stdlib.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdlib.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h: 
+/home/juan/ti/ccsv7/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h: 
 ../LED.hpp: 
